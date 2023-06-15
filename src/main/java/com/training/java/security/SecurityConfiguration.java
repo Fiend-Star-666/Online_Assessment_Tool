b6e0c9bb05a18e4acad73cb6c53791fb2e3c30ae
Fiend-Star-666 <gsingh3_be18@thapar.edu>
2023-06-16 00:30:22 +0530

    testing

--- src/main/java/com/training/java/security/SecurityConfiguration.java
+++ src/main/java/com/training/java/security/SecurityConfiguration.java
@@ -3,12 +3,17 @@ package com.training.java.security;
 
 import com.training.java.security.jwt.AuthEntryPointJwt;
 import com.training.java.security.jwt.AuthTokenFilter;
+
+import jakarta.security.auth.message.config.AuthConfig;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,51 +25,36 @@ import org.springframework.stereotype.Service;
 
 
 import static org.springframework.security.config.Customizer.withDefaults;
-@EnableWebSecurity
+
+@Configuration
 @EnableMethodSecurity
-@Service
 public class SecurityConfiguration {
 
     @Autowired
     UserDetailsService userDetailsService;
 
-
     @Autowired
     private AuthEntryPointJwt unauthorizedHandler;
-
-    private AuthenticationManagerBuilder authManagerBuilder;
 
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
     }
+    //https://github.com/bezkoder/spring-boot-spring-security-jwt-authentication/blob/master/src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public void authenticationManagerBuilder(UserDetailsService userDetailsService) throws Exception {
-        this.authManagerBuilder = new AuthenticationManagerBuilder(userDetailsService);
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        authManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return authManagerBuilder.build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").hasRole("USER")
-                )
-                .formLogin((form) -> form
-                .loginPage("/login")
-                .permitAll()
-                 )
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/**").hasRole("USER"))
+                .formLogin((form) -> form.loginPage("/login").permitAll())
                 .logout((logout) -> logout.permitAll())
                 .httpBasic(withDefaults());
         return http.build();

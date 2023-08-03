72768ae9ac25bd167c2a3bbe087879651c051dce
Gursimran <gurmsi1060@outlook.com>
2023-08-03 21:33:44 +0530

    added the environment variables

--- Back-end/src/main/java/com/training/java/security/jwt/AuthTokenFilter.java
+++ Back-end/src/main/java/com/training/java/security/jwt/AuthTokenFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +32,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     private MyUserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain)
             throws ServletException, IOException {
         try {
 
